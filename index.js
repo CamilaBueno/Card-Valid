fdb01095893b8af3d1221e645335dd3bc2d16ec3
Camila Bueno <cbf.camila@gmail.com>
2018-09-05 09:54:22 -0300

    Alterações nos testes

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 function cardValidator(cardNumber) {
- if (cardNumber === ''){
-   return false;
- }
- if(typeof cardNumber !== 'number') {
-   return false;
- }
-    let result = 0;
-    let multResult;
-  // let cardNumber;
-   let array = [];
-   array.push(cardNumber);
-   array.join('').split('').map(x => parseInt(x)).reverse();
-  
-    //if (!cardNumber || cardNumber.length != 16) {
-
-     // return new Error("Digite corretamente o número do seu cartão de crédito");
-   // }
-  
-    for (var i = 0; i < cardNumber.length; i++) {
-      if (i % 2 !== 0) {
-        if((cardNumber[i] * 2) >= 10) {
-          return cardNumber[i] - 9;
-         
-      
-      } 
-       
-      } else {
-        return cardNumber[i];
-        }
-        result += cardNumber;
-    }
-  
-  
-    if (result % 10 === 0) {
-      
-      return true;
-    } else {
-     
-     return false;
-    }
-  
+  if (cardNumber === '') {
+    return false;
   }
-  
-  module.exports = cardValidator;
+  if (typeof cardNumber !== 'number') {
+    return false;
+  }
+  let result = 0;
+  let multResult;
+
+  cardNumber = cardNumber.toString().split('');
+
+  for(var i = 0; i < cardNumber.length; i++) {
+    if(i % 2 !== 0) {
+      result += parseInt(cardNumber[i]);
+    } else {
+      multResult = cardNumber[i] * 2;
+      if (multResult >= 10) {
+        multResult = multResult - 9;
+      }
+      result += multResult;
+    }
+  }
+
+  if (result % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+module.exports = cardValidator;
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,14 +4,17 @@ const mocha = require("mocha");
 const cardValidator = require('../index');
 //const expect = chai.expect;
 
-describe('cardValidator()', function() {
-    it('Deve retornar true', () => {
-      assert.equal(cardValidator(5234210210864429), true);
-    });
-    it('Deve retornar true', () => {
-      assert.equal(cardValidator(36490102462661), true);
-    });
-it('Deve retornar false', () => {
+describe('cardValidator()', function () {
+  it('Deve retornar true', () => {
+    assert.equal(cardValidator(5234210210864429), true);
+  });
+  it('Deve retornar true', () => {
+    assert.equal(cardValidator(36490102462661), true);
+  });
+  it('Deve retornar false', () => {
+    assert.equal(cardValidator(15975324555466), false);
+  });
+  it('Deve retornar false', () => {
     assert.equal(cardValidator(''), false);
   });
   it('Deve retornar false', () => {
